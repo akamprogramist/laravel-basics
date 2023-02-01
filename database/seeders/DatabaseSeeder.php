6d909195fc9d176f4668c697a62a4d75593d9927
Akam Programist <akadev404@gmail.com>
2023-02-01 13:21:15 +0300

    commits

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +17,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
-        Listing::factory(6)->create();
+        $user = User::factory()->create([
+            'name' => 'jhon doe',
+            'email' => 'jhon@gmail.com',
+
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
         //  Listing::create([
         //     'title'=>'laravel',
         //     'tags' =>'laravel , javascript',
